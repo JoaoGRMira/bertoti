7a83f41f0fdc047fef511c6badda0bf907a685a2
João Gabriel Ribeiro Mira <103254343+JoaoGRMira@users.noreply.github.com>
2022-11-24 16:00:40 -0300

    Add files via upload

---
+++ Engenharia de Software I/bdBancoBertoti.sql
@@ -0,0 +1,9 @@
+create database bancoBertoti;
+use bancoBertoti;
+
+create table usuario(
+id_usuario int primary key auto_increment,
+nome varchar(40),
+rg varchar(30),
+sala varchar(30),
+biometria varchar(30));
